d8517d624a8eb5bd6ca5b24e156eb2f44f03480c
Krystian Igras <krystian8207@gmail.com>
2023-03-16 19:01:19 +0100

    Add sample data, filters and displaying it.

--- R/ui.R
+++ R/ui.R
@@ -5,8 +5,19 @@ ui <- shinydashboardPlus::dashboardPage(
     title = "Cool App" |> shiny::h3(class = "dropdown", style = "display: inline;")
   ),
   sidebar = shinydashboardPlus::dashboardSidebar(
-    minified = TRUE, collapsed = FALSE
+    minified = TRUE, collapsed = FALSE,
+    shiny::selectInput("store", "Store", choices = 1:3, selected = 1),
+    shiny::selectInput("department", "Department", choices = 1:3, selected = 1),
+    shiny::selectInput(
+      "variables", "Variables",
+      choices = c("Date", "Temperature", "Fuel_Price", "CPI", "Unemployment", "IsHoliday", "Weekly_Sales", "Name"),
+      selected = c("Date", "Weekly_Sales"), multiple = TRUE
+    )
   ),
   body = shinydashboard::dashboardBody(
-  )
+    shiny::textOutput("title"),
+    shiny::tableOutput("sales_data")
+  ),
+  controlbar = shinydashboardPlus::dashboardControlbar(),
+  title = "DashboardPage"
 )
